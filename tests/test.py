11deea37684c5429a43e2461c6cb4c86c9c96edc
J. Félix Ontañón <felixonta@gmail.com>
2018-10-28 12:17:27 +0100

    Added a new transcription tests against full lemario. Useful to check whether a rule change introduces improvements or errors.

--- tests/test.py
+++ tests/test.py
@@ -17,11 +17,50 @@ sys.path.insert(0, parent_dir)
 # Now it can be imported :)
 import epa
 
-# Let's test
-assert epa.cas_to_epa('Xenomorfo dice: [haber], que el Éxito y el éxtasis asfixian, si no eres un xilófono Chungo.') == u'Çenomorfo diçe: [abêh], que el Éççito y el éttaçî âffîççian, çi no erê un çilófono Xungo.'
-assert epa.cas_to_epa('Lleva un Guijarrito, para la VERGÜENZA!') == u'Yeba un Giharrito, para la BERGUENÇA!'
-assert epa.cas_to_epa('VALLA valla, si vas de ENVIDIA') == u'BAYA baya, çi bâ de EMBIDIA'
-assert epa.cas_to_epa('Alrededor de la Alpaca había un ALfabeto ALTIVO de valkirias malnacidas') == u'Arrededôh de la Arpaca abía un ARfabeto ARTIBO de barkiriâ mânnaçidâ'
-assert epa.cas_to_epa('En la Zaragoza asexual se Sabía SÉriamente sILBAR') == u'En la Çaragoça açêççuâh çe Çabía ÇÉriamente çIRBÂH'
-assert epa.cas_to_epa('Transportandonos a la connotación del abstracto solsticio de Alaska, el aislante plástico adsorvente asfixió al aMnésico pseudoescritor granadino de constituciones, para ConMemorar broncas adscritas') == u'Trâpportandonô a la cônnotaçión dêh âttrâtto çorttiçio de Alâkka, el aîl-lante pláttico âççorbente âffîççió al ânnéçico çeudoêccritôh granadino de côttituçionê, para CôMMemorâh broncâ âccritâ'
-assert epa.cas_to_epa('Venid a correr en Cádiz con actitud y maldad, para escuchar a Albéniz tocar ápud con virtud de laúd.') == u'Benîh a corrêh en Cádî con âttitûh y mardá, para êccuxâh a Arbénî tocâh ápû con birtûh de laûh.'
+# Basic tests
+def test1():
+    assert epa.cas_to_epa('Xenomorfo dice: [haber], que el Éxito y el éxtasis asfixian, si no eres un xilófono Chungo.') == u'Çenomorfo diçe: [abêh], que el Éççito y el éttaçî âffîççian, çi no erê un çilófono Xungo.'
+    assert epa.cas_to_epa('Lleva un Guijarrito, para la VERGÜENZA!') == u'Yeba un Giharrito, para la BERGUENÇA!'
+    assert epa.cas_to_epa('VALLA valla, si vas de ENVIDIA') == u'BAYA baya, çi bâ de EMBIDIA'
+    assert epa.cas_to_epa('Alrededor de la Alpaca había un ALfabeto ALTIVO de valkirias malnacidas') == u'Arrededôh de la Arpaca abía un ARfabeto ARTIBO de barkiriâ mânnaçidâ'
+    assert epa.cas_to_epa('En la Zaragoza asexual se Sabía SÉriamente sILBAR') == u'En la Çaragoça açêççuâh çe Çabía ÇÉriamente çIRBÂH'
+    assert epa.cas_to_epa('Transportandonos a la connotación del abstracto solsticio de Alaska, el aislante plástico adsorvente asfixió al aMnésico pseudoescritor granadino de constituciones, para ConMemorar broncas adscritas') == u'Trâpportandonô a la cônnotaçión dêh âttrâtto çorttiçio de Alâkka, el aîl-lante pláttico âççorbente âffîççió al ânnéçico çeudoêccritôh granadino de côttituçionê, para CôMMemorâh broncâ âccritâ'
+    assert epa.cas_to_epa('Venid a correr en Cádiz con actitud y maldad, para escuchar a Albéniz tocar ápud con virtud de laúd.') == u'Benîh a corrêh en Cádî con âttitûh y mardá, para êccuxâh a Arbénî tocâh ápû con birtûh de laûh.'
+
+# Lemario test
+def test2(report_all = False):
+
+    import csv
+    file = "./tests/lemario_cas_and.csv"
+
+    transcriptions = []
+    transcription_errors = []
+    stats = {"total": 0, "ok": 0, "fail": 0}
+
+    with open(file) as fh:
+        rd = csv.DictReader(fh, delimiter=',')
+
+        for row in rd:
+            caste = unicode(row['cas'], 'utf-8')
+            andal = unicode(row['and'], 'utf-8')
+            trans = epa.cas_to_epa(row['cas'])
+
+            if andal != trans:
+                transcription_errors.append((caste, andal, trans))
+                stats["fail"] += 1
+            else:
+                stats["ok"] += 1
+
+            transcriptions.append((caste, andal, trans))
+            stats["total"] += 1
+    
+
+    if report_all:
+        for error in transcription_errors:
+            print error[0] + " => " + error[1] + ', ' + error[2]
+    import pprint
+    pprint.pprint(stats)
+
+if __name__ == '__main__':
+    test1()
+    test2(True)
