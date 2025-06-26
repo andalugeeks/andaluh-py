178379c7fd48a03d2ddcaff9c40e9a9afbda6a03
Sergio Soto <ssoto@freepik.com>
2025-06-26 18:51:00 +0200

    refactor: Preparing package for 1.0.0 release

--- Makefile
+++ Makefile
@@ -1,20 +1,48 @@
+.PHONY: create-venv activate-venv install setup-build-tools build publish clean
 
-setup-build-tools:
+VENV_PYTHON=.venv/bin/python3
+PYTHON_TESTS=.venv/bin/pytest
+
+help: ## Muestra esta ayuda
+	@echo "Ayuda: make <target>"
+	@echo "Targets disponibles:"
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
+
+create-venv: ## Crea un entorno virtual
+	@echo "Creando entorno virtual..."
+	@$(VENV_PYTHON) -m venv .venv
+	@echo "Entorno virtual creado en .venv"
+
+install: create-venv setup-build-tools ## Instala el módulo andaluh en modo desarrollo
+	@echo "Instalando módulo andaluh..."
+	@$(VENV_PYTHON) -m pip install -e .
+	@echo "Módulo andaluh instalado en modo desarrollo"
+
+setup-build-tools: ## Configura las herramientas de construcción
 	@echo "Setting up build tools..."
-	@. .venv/bin/activate && python -m pip install build twine
+	@$(VENV_PYTHON) -m pip install build twine
 
-build:
+build: create-venv ## Construye el paquete
 	@echo "Building..."
 	@if [ -d "dist" ]; then \
 		echo "WARNING: Clean dist directory first."; \
 		exit 1; \
 	fi
-	@python -m build
+	@$(VENV_PYTHON) -m build
 
-publish:
+publish: create-venv ## Publica el paquete
 	@echo "Publishing..."
-	@. .venv/bin/activate && twine upload dist/*
+	@twine upload dist/*
 
-clean:
+clean: create-venv ## Limpia el entorno
 	@echo "Cleaning..."
 	@rm -rf dist build *.egg-info
+
+dev-dependencies: create-venv ## Instala las dependencias de desarrollo
+	@echo "Installing development dependencies..."
+	@$(VENV_PYTHON) -m pip install -r dev-requirements.txt
+	@echo "Development dependencies installed"
+
+run-tests: dev-dependencies ## Ejecuta los tests
+	@echo "Running tests..."
+	@$(PYTHON_TESTS)
--- tests/fn_test.py
+++ tests/fn_test.py
@@ -9,61 +9,56 @@
 # - J. Félix Ontañón <felixonta@gmail.com>
 # - Sergio Soto <scots4ever@gmail.com>
 
-# Import package form parent dir https://gist.github.com/JungeAlexander/6ce0a5213f3af56d7369
-import os,sys,inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
+import csv
+import pprint
+import pytest
 
-# Now it can be imported :)
 import andaluh
 
-# Basic tests
-def test1():
-    assert andaluh.epa('Todo Xenomorfo dice: [haber], que el Éxito y el éxtasis asfixian, si no eres un xilófono Chungo.') == 'Tó Çenomorfo diçe: [abêh], que el Éççito y el éttaçî âffîççian, çi no erê un çilófono Xungo.'
-    assert andaluh.epa('Lleva un Guijarrito el ABuelo, ¡Qué Bueno! ¡para la VERGÜENZA!') == 'Yeba un Giharrito el AGuelo, ¡Qué Gueno! ¡pa la BERGUENÇA!'
-    assert andaluh.epa('VALLA valla, si vas toda de ENVIDIA') == 'BAYA baya, çi bâ toa de EMBIDIA'
-    assert andaluh.epa('Alrededor de la Alpaca había un ALfabeto ALTIVO de valkirias malnacidas') == 'Arrededôh de la Arpaca abía un ARfabeto ARTIBO de barkiriâ mânnaçidâ'
-    assert andaluh.epa('En la Zaragoza y el Japón asexual se Sabía SÉriamente sILBAR con el COxis') == 'En la Çaragoça y er Hapón açêççuâh çe Çabía ÇÉriamente çIRBÂH con er CÔççî'
-    assert andaluh.epa('Transportandonos a la connotación perspicaz del abstracto solsticio de Alaska, el aislante plástico adsorvente asfixió al aMnésico pseudoescritor granadino de constituciones, para ConMemorar broncas adscritas') == 'Trâpportandonô a la cônnotaçión perppicâh del âttrâtto çorttiçio de Alâkka, el aîl-lante pláttico âççorbente âffîççió al ânnéçico çeudoêccritôh granadino de côttituçionê, pa CôMMemorâh broncâ âccritâ'
-    assert andaluh.epa('En la postmodernidad, el transcurso de los transportes y translados en postoperatorios transcienden a la postre unas postillas postpalatales apostilladas se transfieren') == 'En la pômmodênnidá, er trâccurço de lô trâpportê y trâl-láô en pôttoperatoriô trâççienden a la pôttre unâ pôttiyâ pôppalatalê apôttiyâh çe trâffieren'
-    assert andaluh.epa('Venid todos a correr en anorak de visón a Cádiz con actitud y maldad, para escuchar el tríceps de Albéniz tocar ápud con virtud de laúd.') == 'Benîh tôh a corrêh en anorâh de biçón a Cádî con âttitûh y mardá, pa êccuxâh er tríçê de Arbénî tocâh ápû con birtûh de laûh.'
-    assert andaluh.epa('Una comida fabada con fado, y sin descuido será casada y amarrada al acolchado roido.') == 'Una comida fabada con fado, y çin dêccuido çerá caçá y amarrá al acorxao roío.'
-    assert andaluh.epa('Los SABuesos ChiHuaHUA comían cacaHuETes, FramBuESas y Heno, ¡y HABLAN con hálito de ESPANGLISH!') == 'Lô ÇAGueçô XiGuaGUA comían cacaGuETê, FramBuEÇâ y Eno, ¡y ABLAN con álito de ÊPPANGLÎ!'
-    assert andaluh.epa('Oye sexy psiquiatra @miguel, la #web HTTPS://andaluh.es/transcriptor no sale en google.es pero si en http://google.com?utm=13_123.html #porqueseñor', escape_links=True) == 'Oye çêççy çiquiatra @miguel, la #web HTTPS://andaluh.es/transcriptor no çale en google.es pero çi en http://google.com?utm=13_123.html #porqueseñor'
-# Lemario test
-def test2(report_all = False):
+# Test cases fixture
+@pytest.fixture(params=[
+    ('Todo Xenomorfo dice: [haber], que el Éxito y el éxtasis asfixian, si no eres un xilófono Chungo.',
+     'Tó Çenomorfo diçe: [abêh], que el Éççito y el éttaçî âffîççian, çi no erê un çilófono Xungo.'),
+    ('Lleva un Guijarrito el ABuelo, ¡Qué Bueno! ¡para la VERGÜENZA!',
+     'Yeba un Giharrito el AGuelo, ¡Qué Gueno! ¡pa la BERGUENÇA!'),
+    ('VALLA valla, si vas toda de ENVIDIA',
+     'BAYA baya, çi bâ toa de EMBIDIA'),
+    ('Alrededor de la Alpaca había un ALfabeto ALTIVO de valkirias malnacidas',
+     'Arrededôh de la Arpaca abía un ARfabeto ARTIBO de barkiriâ mânnaçidâ'),
+    ('En la Zaragoza y el Japón asexual se Sabía SÉriamente sILBAR con el COxis',
+     'En la Çaragoça y er Hapón açêççuâh çe Çabía ÇÉriamente çIRBÂH con er CÔççî'),
+    ('Transportandonos a la connotación perspicaz del abstracto solsticio de Alaska, el aislante plástico adsorvente asfixió al aMnésico pseudoescritor granadino de constituciones, para ConMemorar broncas adscritas',
+     'Trâpportandonô a la cônnotaçión perppicâh del âttrâtto çorttiçio de Alâkka, el aîl-lante pláttico âççorbente âffîççió al ânnéçico çeudoêccritôh granadino de côttituçionê, pa CôMMemorâh broncâ âccritâ'),
+    ('Venid todos a correr en anorak de visón a Cádiz con actitud y maldad, para escuchar el tríceps de Albéniz tocar ápud con virtud de laúd.',
+     'Benîh tôh a corrêh en anorâh de biçón a Cádî con âttitûh y mardá, pa êccuxâh er tríçê de Arbénî tocâh ápû con birtûh de laûh.'),
+    ('Una comida fabada con fado, y sin descuido será casada y amarrada al acolchado roido.',
+     'Una comida fabada con fado, y çin dêccuido çerá caçá y amarrá al acorxao roío.'),
+    ('Los SABuesos ChiHuaHUA comían cacaHuETes, FramBuESas y Heno, ¡y HABLAN con hálito de ESPANGLISH!',
+     'Lô ÇAGueçô XiGuaGUA comían cacaGuETê, FramBuEÇâ y Eno, ¡y ABLAN con álito de ÊPPANGLÎ!'),
+])
+def test_case(request):
+    return request.param
 
-    import csv
-    file = "./tests/lemario_cas_and.csv"
+# Test cases with special parameters
+@pytest.fixture(params=[
+    ('Oye sexy psiquiatra @miguel, la #web HTTPS://andaluh.es/transcriptor no çale en google.es pero çi en http://google.com?utm=13_123.html #porqueseñor',
+     'Oye çêççy çiquiatra @miguel, la #web HTTPS://andaluh.es/transcriptor no çale en google.es pero çi en http://google.com?utm=13_123.html #porqueseñor',
+     {'escape_links': True}),
+])
+def test_case_with_params(request):
+    return request.param
 
-    transcriptions = []
-    transcription_errors = []
-    stats = {"total": 0, "ok": 0, "fail": 0}
+# Basic transcription tests
+def test_andaluh_transcription(test_case):
+    """Test basic andaluh transcription cases"""
+    input_text, expected_output = test_case
+    result = andaluh.epa(input_text)
+    assert result == expected_output, f"Input: {input_text}\nExpected: {expected_output}\nGot: {result}"
 
-    with open(file) as fh:
-        rd = csv.DictReader(fh, delimiter=',')
+# Transcription tests with parameters
+def test_andaluh_transcription_with_params(test_case_with_params):
+    """Test andaluh transcription cases with special parameters"""
+    input_text, expected_output, params = test_case_with_params
+    result = andaluh.epa(input_text, **params)
+    assert result == expected_output, f"Input: {input_text}\nExpected: {expected_output}\nGot: {result}"
 
-        for row in rd:
-            caste = row['cas']
-            andal = row['and']
-            trans = andaluh.epa(row['cas'])
-
-            if andal != trans:
-                transcription_errors.append((caste, andal, trans))
-                stats["fail"] += 1
-            else:
-                stats["ok"] += 1
-
-            transcriptions.append((caste, andal, trans))
-            stats["total"] += 1
-    
-
-    if report_all:
-        for error in transcription_errors:
-            print(error[0] + " => " + error[1] + ', ' + error[2])
-    import pprint
-    pprint.pprint(stats)
-
-if __name__ == '__main__':
-    test1()
