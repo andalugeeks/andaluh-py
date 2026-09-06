d2f68244b7184221194be65681f2baf539daaec8
Arbonaida <andres.de.xerez@gmail.com>
2026-09-06 03:40:13 +0200

    refactor(package): exporta las funciones de contracciones desde __init__.py

--- andaluh/__init__.py
+++ andaluh/__init__.py
@@ -10,7 +10,26 @@
 # - Sergio Soto <scots4ever@gmail.com>
 
 from .lib import epa
+from .de import transformar_preposicion_de
+from .en import transformar_preposicion_en
+from .pa import transformar_preposicion_pa
+from .ya import transformar_adverbio_ya
+from .pronombres import transformar_pronombres_atonos
+from .articulos import (transformar_articulos,
+                        transformar_articulo_el,
+                        transformar_articulo_la, )
+from .contractions import apply_contractions, aplicar_contracciones
 
 __all__ = [
     'epa',
+    'transformar_preposicion_de',
+    'transformar_preposicion_en',
+    'transformar_preposicion_pa',
+    'transformar_adverbio_ya',
+    'transformar_pronombres_atonos',
+    'transformar_articulos',
+    'transformar_articulo_el',
+    'transformar_articulo_la',
+    'apply_contractions',
+    'aplicar_contracciones',
 ]
